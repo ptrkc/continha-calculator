40d332235f76c20bbc427fd61962256ef62fae12
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2022-11-02 02:44:47 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <div className="flex flex-col mx-auto max-w-2xl border-2 border-black">
+        <p>Add person+</p>
+        <p>Add product+</p>
+        <p>Total:</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
